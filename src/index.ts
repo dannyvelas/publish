b703edd41fa0be381944521f11248aa85749a804
Daniel Velasquez <danivelas4@gmail.com>
2024-04-11 23:38:41 -0400

    src/index.ts: added support for optionally not deleting files

--- src/index.ts
+++ src/index.ts
@@ -5,24 +5,29 @@ import remarkFrontmatter from "remark-frontmatter";
 import remarkGfm from "remark-gfm";
 import matter from "gray-matter";
 import fsp from "fs/promises";
+import { Dirent } from "fs";
 import { wikiLinkPlugin } from "./wiki-link-plugin/index.js";
 import { File } from "./types.js";
 import path from "path";
+import readline from "readline/promises";
 
 const inDir = `/Users/dannyvelasquez/RemoteGit/MyGithub/notes/`;
 const outDir = `/Users/dannyvelasquez/RemoteGit/MyGithub/My-Websites/my-second-website/src/pages/posts/`;
 
 async function main() {
   // find all markdown files recursively in current directory
-  const filePaths = await fsp.readdir(inDir, { recursive: true });
-  const mdFilePaths = filePaths.filter((x) => x.endsWith(".md"));
+  const filePaths = await fsp.readdir(inDir, {
+    recursive: true,
+    withFileTypes: true,
+  });
+  const mdFilePaths = filePaths.filter((x) => x.name.endsWith(".md"));
 
   // convert markdown files to `File` objects
   const mdFiles = await Promise.all(mdFilePaths.map(newFile));
   const mdFilesOk = mdFiles.filter((x): x is File => x !== undefined);
 
   // if the markdown file is not public, ignore
-  const publicMdFiles = mdFilesOk.filter((x) => x.data.public === true);
+  const publicMdFiles = mdFilesOk.filter((x) => x.data.publish === true);
 
   // collect permalinks
   const permalinks = publicMdFiles.map((x) => x.name);
@@ -32,14 +37,7 @@ async function main() {
     publicMdFiles.map((x) => blogify(x, permalinks))
   );
 
-  // delete all old posts to avoid the `outDir` from becoming filled with deleted files
-  const oldPosts = await fsp.readdir(outDir, {
-    recursive: true,
-    withFileTypes: true,
-  });
-  for (const post of oldPosts) {
-    await fsp.rm(path.join(post.path, post.name));
-  }
+  deleteOldPostsIfNecessary(newPosts);
 
   // write new posts to `outDir`
   for (const post of newPosts) {
@@ -48,15 +46,20 @@ async function main() {
   }
 }
 
-async function newFile(filePath: string): Promise<File | undefined> {
-  // read file at path
-  const content = await fsp.readFile(filePath, { encoding: "utf8" });
+async function newFile(dirent: Dirent): Promise<File | undefined> {
+  const fullPath = path.join(dirent.path, dirent.name);
 
-  const parsedPath = path.parse(filePath);
+  // read file at path
+  const content = await fsp.readFile(fullPath, { encoding: "utf8" });
+
+  // translate path to an object with useful fields such as `base, ext, dir`
+  const parsedPath = path.parse(fullPath);
 
   return { ...matter(content), ...parsedPath, content };
 }
 
+// blogify takes a file and returns the same exact file, except content will be the
+// transformed markdown
 async function blogify(file: File, permalinks: string[]): Promise<File> {
   return {
     ...file,
@@ -82,6 +85,39 @@ async function transformMarkdown(
 
   const file = await processor.process(md);
   return file.toString();
+}
+
+async function deleteOldPostsIfNecessary(newPosts: File[]) {
+  // get old posts
+  const oldPosts = await fsp.readdir(outDir, {
+    recursive: true,
+    withFileTypes: true,
+  });
+
+  // postsToDelete = posts that are in set of `oldPosts` that are not in the set of `newPosts`
+  const newPostsSet = new Set(newPosts.map((x) => x.base));
+  const postsToDelete = oldPosts.filter((x) => !newPostsSet.has(x.name));
+
+  // if all old posts are also new posts, we can safely return early
+  if (postsToDelete.length === 0) {
+    return;
+  }
+
+  // otherwise, initialize rl interface so that we can ask user if we want to delete old files
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  // for each post, make sure user is consenting to delete the old post
+  for (const post of postsToDelete) {
+    const ans = await rl.question(`delete ${post.name}? `);
+    const lowerCaseAns = ans.toLowerCase();
+    if (lowerCaseAns === "y" || lowerCaseAns === "yes") {
+      await fsp.rm(path.join(post.path, post.name));
+    }
+  }
+  rl.close();
 }
 
 main();
