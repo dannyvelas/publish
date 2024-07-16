12ea5efb220f3d2acf7044cce398f3887092eabd
Daniel Velasquez <danivelas4@gmail.com>
2024-07-16 16:13:47 -0400

    was able to get tests working with vitest A babel.config.cjs D jest.config.js M package-lock.json M package.json M src/process.test.ts

--- src/process.test.ts
+++ src/process.test.ts
@@ -1,43 +1,50 @@
 import { transformMarkdown } from "./process.js";
+import { test, expect } from 'vitest';
 import dedent from "dedent-js";
 
 test.each([
   {
     name: "transformMarkdown adds a layout field to the frontmatter",
-    input: dedent`---
-      title: "The importance of good testing"
-      date: 2024-04-17
-      publish: true
-      ---
-      Example text`,
-    expected: dedent`---
-      title: "The importance of good testing"
-      date: 2024-04-17
-      publish: true
-      layout: "../../layouts/BlogLayout.astro"
-      ---
-      Example text`,
+    input: `---
+title: "The importance of good testing"
+date: 2024-04-17
+publish: true
+---
+Example text`,
+    expected: `---
+title: "The importance of good testing"
+date: 2024-04-17
+publish: true
+layout: ../../layouts/BlogLayout.astro
+
+---
+
+Example text
+`,
   },
   {
     name: "transformMarkdown will find a paragraph that matches obsidian tag syntax, remove it, and add its information to the frontmatter",
-    input: dedent`---
-      title: "The importance of good testing"
-      date: 2024-04-17
-      publish: true
-      ---
-      Tags: [[career]], [[programming-languages]]
-      
-      Example text`,
-    expected: dedent`---
-      title: "The importance of good testing"
-      date: 2024-04-17
-      tags:
-        - career
-        - programming-languages
-      publish: true
-      ---
-      
-      Example text`,
+    input: `---
+title: "The importance of good testing"
+date: 2024-04-17
+publish: true
+---
+Tags: [[career]], [[programming-languages]]
+
+Example text`,
+    expected: `---
+title: "The importance of good testing"
+date: 2024-04-17
+publish: true
+layout: ../../layouts/BlogLayout.astro
+tags:
+  - career
+  - programming-languages
+
+---
+
+Example text
+`,
   },
 ])("$name", async ({ input, expected }) => {
   const actualMd = await transformMarkdown(input, []);
@@ -49,17 +56,15 @@ test.each([
     name: "transformMarkdown converts a wikilink to text when target is not in permalinks",
     input: "[[nonexisting-link|link alias]]",
     permalinks: [],
-    expected: "link alias",
+    expected: "link alias\n",
   },
   {
     name: "transformMarkdown converts a wikilink to an href when target is in permalinks",
     input: "[[existing-link|link alias]]",
     permalinks: ["existing-link"],
-    expected: "[link alias](/posts/existing-link/)",
+    expected: "[link alias](/posts/existing-link/)\n",
   },
 ])("$name", async ({ input, permalinks, expected }) => {
   const actualMd = await transformMarkdown(input, permalinks);
   expect(actualMd).toBe(expected);
 });
-
-export {};
