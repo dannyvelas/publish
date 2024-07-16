a0b8b866c629e547c748060ace301830499be820
Daniel Velasquez <danivelas4@gmail.com>
2024-07-16 17:01:48 -0400

    src/obsidian-tags-plugin/index.ts: added logic to remove tags: paragraph

--- src/obsidian-tags-plugin/index.ts
+++ src/obsidian-tags-plugin/index.ts
@@ -5,34 +5,35 @@ export default function () {
   return function (root: Root) {
     // find ymlNode
     const ymlNode = root.children.find(
-      (node): node is Yaml => node.type === "yaml"
+      (node): node is Yaml => node.type === "yaml",
     );
-    if (!ymlNode) {
-      return;
-    }
+    if (!ymlNode) return;
 
     // create document from yml
     const frontmatter = parseDocument(ymlNode.value);
 
     // find paragraph that starts with `tags:` (case-insensitive)
-    const tagsParagraphNode = root.children.find((node): node is Paragraph => {
-      if (node.type !== "paragraph" || node.children.length === 0) {
-        return false;
-      }
-      const firstChild = node.children[0];
-      if (firstChild.type !== "text") {
-        return false;
-      }
-      return firstChild.value.toLowerCase().startsWith("tags:");
-    });
-    if (!tagsParagraphNode) {
+    const tagsParagraphIndex = root.children.findIndex(
+      (node): node is Paragraph => {
+        if (node.type !== "paragraph" || node.children.length === 0) {
+          return false;
+        }
+        const firstChild = node.children[0];
+        if (firstChild.type !== "text") {
+          return false;
+        }
+        return firstChild.value.toLowerCase().startsWith("tags:");
+      },
+    );
+    if (tagsParagraphIndex === -1) {
       return;
     }
 
     // get all of the wiki link nodes from that paragraph
+    const tagsParagraphNode = root.children[tagsParagraphIndex] as Paragraph;
     const wikiLinkNodes = tagsParagraphNode.children.filter(
       // @ts-ignore: hack: the `Paragraph` type doesn't recognize that it can have `wikiLink` node children
-      (node) => node.type === "wikiLink"
+      (node) => node.type === "wikiLink",
     );
 
     // extract text from these wiki link nodes
@@ -43,5 +44,8 @@ export default function () {
 
     // update yaml node
     ymlNode.value = frontmatter.toString();
+
+    // remove tagsParagraphNode from root.children. its not needed anymore
+    root.children.splice(tagsParagraphIndex, 1);
   };
 }
